ed8c448d49d2e57cbd229aeb3f0e00632fdc7dac
Nahuel <nahuel18.personal@gmail.com>
2023-10-09 20:38:59 -0300

    uwu

---
+++ views/test.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Horarios de Grupos</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Horarios de Grupos</h1>
+        <table class="horarios-table">
+            <tr>
+                <th>Grupo</th>
+                <th>Salón</th>
+                <th>Día</th>
+                <th>Hora Inicio</th>
+                <th>Hora Fin</th>
+            </tr>
+            <tr>
+                <td>Grupo1</td>
+                <td>SalonA</td>
+                <td>Lunes</td>
+                <td>09:00</td>
+                <td>09:20</td>
+            </tr>
+            <tr>
+                <td>Grupo1</td>
+                <td>SalonB</td>
+                <td>Martes</td>
+                <td>09:00</td>
+                <td>09:20</td>
+            </tr>
+            <tr>
+                <td>Grupo2</td>
+                <td>SalonB</td>
+                <td>Miércoles</td>
+                <td>09:00</td>
+                <td>09:20</td>
+            </tr>
+            <tr>
+                <td>Grupo2</td>
+                <td>SalonA</td>
+                <td>Lunes</td>
+                <td>09:00</td>
+                <td>09:20</td>
+            </tr>
+            <tr>
+                <td>Grupo3</td>
+                <td>SalonC</td>
+                <td>Lunes</td>
+                <td>09:30</td>
+                <td>09:50</td>
+            </tr>
+            <tr>
+                <td>Grupo3</td>
+                <td>SalonD</td>
+                <td>Martes</td>
+                <td>09:30</td>
+                <td>09:50</td>
+            </tr>
+            <tr>
+                <td>Grupo4</td>
+                <td>SalonD</td>
+                <td>Miércoles</td>
+                <td>09:30</td>
+                <td>09:50</td>
+            </tr>
+            <tr>
+                <td>Grupo4</td>
+                <td>SalonC</td>
+                <td>Lunes</td>
+                <td>09:30</td>
+                <td>09:50</td>
+            </tr>
+            <tr>
+                <td>Grupo5</td>
+                <td>SalonE</td>
+                <td>Lunes</td>
+                <td>10:00</td>
+                <td>10:20</td>
+            </tr>
+            <tr>
+                <td>Grupo5</td>
+                <td>SalonF</td>
+                <td>Martes</td>
+                <td>10
+            
+        </table>
+    </div>
+</body>
+</html>
